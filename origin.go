b4d0725a40d887fb3a9642535e7d7299f6db8c12
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2018-01-22 15:33:42 -0800

    Origin now stores a parsed URL

--- origin.go
+++ origin.go
@@ -1,12 +1,58 @@
 package dpmafirmware
 
+import (
+	"encoding/json"
+	"net/url"
+)
+
+// VersionPlaceholder is the placeholder text in a tarball path that should be
+// replaced with the version number of a particular release.
+const VersionPlaceholder = "{version}"
+
 // Origin describes an origin from which dpma firmware can be downloaded.
 type Origin struct {
-	URL     string `json:"path"` // Root URL
-	Tarball string `json:"tarball"`
+	Base    *url.URL `json:"path"`    // Base URL
+	Tarball string   `json:"tarball"` // Tarball path relative to base
+}
+
+// String returns a string representation of the origin.
+func (o *Origin) String() string {
+	return o.Base.String() + o.Tarball
+}
+
+// UnmarshalJSON unmarshals the origin from JSON-encoded data.
+func (o *Origin) UnmarshalJSON(b []byte) error {
+	raw := struct {
+		Base    string `json:"path"`
+		Tarball string `json:"tarball"`
+	}{}
+	json.Unmarshal(b, &raw)
+
+	var err error
+
+	// Ensure that the base URL can be parsed
+	o.Base, err = url.Parse(raw.Base)
+	if err != nil {
+		return err
+	}
+
+	// Also verify that the tarball can be parsed, even though we store it as a
+	// string
+	_, err = url.Parse(raw.Tarball)
+	if err != nil {
+		return err
+	}
+	o.Tarball = raw.Tarball
+
+	// Enforce HTTPS (does this belong here?)
+	if o.Base.Scheme == "http" {
+		o.Base.Scheme = "https"
+	}
+
+	return nil
 }
 
 func (o *Origin) marshalRawJSON(raw map[string]interface{}) {
-	raw["path"] = o.URL
+	raw["path"] = o.Base.String()
 	raw["tarball"] = o.Tarball
 }
